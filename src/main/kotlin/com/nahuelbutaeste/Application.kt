641494524ca40cb4b1cd24d61fd1ca5a4e836720
Flopy <f.hidalgoalarcon@gmail.com>
2023-10-09 22:11:46 -0300

    Creación de proyecto con implementación de zip ktor.

---
+++ src/main/kotlin/com/nahuelbutaeste/Application.kt
@@ -0,0 +1,15 @@
+package com.nahuelbutaeste
+
+import com.nahuelbutaeste.plugins.*
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureSecurity()
+    configureRouting()
+}
---
+++ src/main/kotlin/com/nahuelbutaeste/plugins/Routing.kt
@@ -0,0 +1,20 @@
+package com.nahuelbutaeste.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        // Static plugin. Try to access `/static/index.html
+        static("/static") {
+            resources("/static")
+        }
+    }
+}
+
+
